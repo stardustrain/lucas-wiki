e1a84621f23733da1e15126213e52334573f4bd0
stardustrain <kthanterran@gmail.com>
2021-01-04 20:26:04 +0900

    :wrench: Fix gatsby-plugin-google-analytics setting

--- gatsby-config.js
+++ gatsby-config.js
@@ -98,7 +98,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        // trackingId: process.env.GA_TRACKING_ID,
+        trackingId: process.env.GA_TRACKING_ID,
       },
     },
     {
