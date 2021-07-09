6db43a760d0bcac3e735f20e8f5c51d820b4731a
stardustrain <kthanterran@gmail.com>
2021-07-09 10:53:02 +0900

    Issue-46 Fix filter selectors style

--- src/components/SortFilter.tsx
+++ src/components/SortFilter.tsx
@@ -1,10 +1,65 @@
 import React from 'react'
 import styled from '@emotion/styled'
+import isPropValid from '@emotion/is-prop-valid'
 
-import Button from './Button'
+import { useFilterContext, SortOption } from '../contexts/FilterContext'
 
-interface Props {}
+import DefaultButton from './Button'
+
+const Div = styled.div`
+  display: inline-flex;
+
+  & > :not(style) ~ :not(style) {
+    margin-inline-start: 5px;
+  }
+
+  @media (max-width: 30rem) {
+    width: 100%;
+    margin-top: 8px;
+  }
+`
+
+const Button = styled(DefaultButton, {
+  shouldForwardProp: props => isPropValid(props),
+})<{ isSelected: boolean }>`
+  background-color: ${({ theme, isSelected }) =>
+    isSelected ? theme.color.button.hover : theme.color.button.background};
+
+  @media (max-width: 30rem) {
+    width: 50%;
+  }
+`
 
 export default function SortFilter() {
-  return <div className="SortFilter"></div>
+  const {
+    state: { sortOption },
+    dispatch,
+  } = useFilterContext()
+
+  return (
+    <Div>
+      <Button
+        isSelected={sortOption === SortOption.DESC}
+        onClick={() => {
+          dispatch({
+            type: 'SET_SORT_OPTION',
+            payload: SortOption.DESC,
+          })
+        }}
+      >
+        최신순
+      </Button>
+      <Button
+        isSelected={sortOption === SortOption.ASC}
+        onClick={() => {
+          dispatch({
+            type: 'SET_SORT_OPTION',
+            payload: SortOption.ASC,
+          })
+        }}
+      >
+        등록순
+      </Button>
+    </Div>
+  )
 }
