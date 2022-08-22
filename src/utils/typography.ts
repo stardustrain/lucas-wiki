37de2b8b3d8cc9fd05cbdc1491290cbd8a788286
stardustrain <kthanterran@gmail.com>
2022-08-22 23:54:17 +0900

    :art:  add Pretendard font

--- src/utils/typography.ts
+++ src/utils/typography.ts
@@ -1,10 +1,14 @@
 import Typography from 'typography'
+import type {TypographyOptions} from 'typography'
 import GithubTheme from 'typography-theme-github'
 
-GithubTheme.overrideThemeStyles = () => ({
+(GithubTheme as TypographyOptions).overrideStyles = (_, options) => ({
   h1: {
     borderBottom: '0',
   },
+  'h1,h2,h3,h4,h5,h6': {
+    fontFamily: 'Pretendard',
+  }
 })
 
 const typography = new Typography(GithubTheme)
