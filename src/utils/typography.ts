34a3c50673ccabd785c1c03ada5b8fd58a489db0
stardustrain <kthanterran@gmail.com>
2022-08-24 21:52:10 +0900

    :art:  remove Pretendard font

--- src/utils/typography.ts
+++ src/utils/typography.ts
@@ -5,9 +5,6 @@ import GithubTheme from 'typography-theme-github'
 (GithubTheme as TypographyOptions).overrideStyles = (_, options) => ({
   h1: {
     borderBottom: '0',
-  },
-  'h1,h2,h3,h4,h5,h6': {
-    fontFamily: 'Pretendard',
   }
 })
 
